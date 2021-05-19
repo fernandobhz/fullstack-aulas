f6e11e032ff4b5d4d82671a09199699fd167b325
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-05-18 21:22:09 -0300

    chore: more examples

--- CineHouse/cinema.js
+++ CineHouse/cinema.js
@@ -41,11 +41,50 @@ function imprimirFilmePelaPosicao(posicaoDoFilmeNaArray) {
   console.log(textoDescritivoDoFilme);
 }
 
+function imprimirFilmePeloCodigo(codigoFilme) {
+  const filme = catalogo.find(filme.codigo === codigoFilme);
+
+  // 1: Titanic, duração de 3 hora(s), atores: Kate Winslet, Leonardo DiCaprio, lançado em 1998, em cartaz: [simmm! | naaaão]
+  const atores = filme.atores.join(", ");
+
+  const textoDescritivoDoFilme = `
+    Filme:
+    ------
+    Código: ${filme.codigo},
+    Título: ${filme.titulo} ${filme.duracao} horas(s),
+    Atores: ${atores},
+    Lancado em ${filme.anoDeLancamento},
+    Em cartaz: ${filme.emCartaz ? "Sim" : "Não"}
+
+  `;
+
+  console.log(textoDescritivoDoFilme);
+}
+
 const listarTodosOsFilmes = () => {
-  for (let idx = 0; idx < catalogo.length; idx++) {
-    imprimirFilmePelaPosicao(idx);
-  }
+  catalogo.forEach((filme, idx) => imprimirFilmePelaPosicao(idx));
 };
+
+const listarFilmesDeLongaDuracao = () => {
+  catalogo.filter((filme, idx) => {
+    if (filme.duracao >= 2) imprimirFilmePelaPosicao(idx);
+  });
+};
+
+const listarFilmesDeLongaDuracao2 = () => {
+  const filmesFiltrados = catalogo.filter((filme) => filme.duracao >= 2);
+
+  filmesFiltrados.forEach((filme) => {
+    imprimirFilmePelaPosicao(filme.codigo - 1);
+  });
+};
+
+const listarFilmesDeLongaDuracaoPeloCodigo = () =>
+  catalogo
+    .filter((filme) => filme.duracao >= 2)
+    .forEach((filme) => {
+      imprimirFilmePeloCodigo(filme.codigo);
+    });
 
 const listarFilmesEmCartaz = () => {
   for (let idx = 0; idx < catalogo.length; idx++) {
@@ -59,12 +98,11 @@ const listarFilmesEmCartaz = () => {
 
 function buscarFilme(posicaoDoFilmeNaArray, callback) {
   const filme = catalogo[posicaoDoFilmeNaArray];
-  setTimeout(callback, 5000, filme)
+  setTimeout(callback, 5000, filme);
 }
 
-
-function alterarStatusEmCartaz(posicaoDoFilmeNaArray) {  
-  const filme = buscarFilme(posicaoDoFilmeNaArray, function(filme) {
+function alterarStatusEmCartaz(posicaoDoFilmeNaArray) {
+  const filme = buscarFilme(posicaoDoFilmeNaArray, function (filme) {
     const invertendoValorEmCartaz = !filme.emCartaz;
     filme.emCartaz = invertendoValorEmCartaz;
   });
@@ -78,4 +116,5 @@ module.exports = {
   listarTodosOsFilmes,
   listarFilmesEmCartaz,
   alterarStatusEmCartaz,
+  listarFilmesDeLongaDuracao,
 };
--- index.js
+++ index.js
@@ -1,34 +1,51 @@
 const cineHouse = require("./CineHouse/cinema.js");
 
-cineHouse.adicionarFilme(
-  3,
-  "Matrix",
-  3,
-  ["Keanu Reeves", "Laurence Fishburne"],
-  1999,
-  true
-);
+function todos() {
+  cineHouse.adicionarFilme(
+    3,
+    "Matrix",
+    3,
+    ["Keanu Reeves", "Laurence Fishburne"],
+    1999,
+    true
+  );
 
-const posicaoDesejada = 1;
-console.log(`Filme ${posicaoDesejada}`);
-cineHouse.imprimirFilmePelaPosicao(posicaoDesejada);
-console.log();
+  const posicaoDesejada = 1;
+  console.log(`Filme ${posicaoDesejada}`);
+  cineHouse.imprimirFilmePelaPosicao(posicaoDesejada);
+  console.log();
 
-
-console.log(`
+  console.log(`
 
   Alterado propriedade em emCartaz do filme na posição ${posicaoDesejada}
 
 `);
 
-cineHouse.alterarStatusEmCartaz(posicaoDesejada);
-cineHouse.imprimirFilmePelaPosicao(posicaoDesejada);
-console.log();
+  cineHouse.alterarStatusEmCartaz(posicaoDesejada);
+  cineHouse.imprimirFilmePelaPosicao(posicaoDesejada);
+  console.log();
 
-console.log('Todos os filmes');
-cineHouse.listarTodosOsFilmes();
-console.log();
+  console.log("Todos os filmes");
+  cineHouse.listarTodosOsFilmes();
+  console.log();
 
-console.log('Filmes em Cartaz');
-cineHouse.listarFilmesEmCartaz();
-console.log();
+  console.log("Filmes em Cartaz");
+  cineHouse.listarFilmesEmCartaz();
+  console.log();
+}
+
+function novo() {
+  console.log("Filmes de Longa Duracao");
+  cineHouse.listarFilmesDeLongaDuracao();
+  console.log();
+}
+
+function novo2() {
+  console.log("Filmes de Longa Duracao");
+  cineHouse.listarFilmesDeLongaDuracao2();
+  console.log();
+}
+
+if (process.argv[2] == "todos") todos();
+if (process.argv[2] == "novo") novo();
+if (process.argv[2] == "novo2") novo2();
